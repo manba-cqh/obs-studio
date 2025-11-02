7daf8c9e0a7f3d04678e1a6607ea0cec7484362d
cuiqinghua <1131205877@qq.com>
2025-11-02 19:29:05 +0800

    更新

--- cmake/common/versionconfig.cmake
+++ cmake/common/versionconfig.cmake
@@ -21,7 +21,18 @@ if(NOT DEFINED OBS_VERSION_OVERRIDE AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git
   endif()
 
   if(_obs_version_result EQUAL 0)
-    string(REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+).*" "\\1;\\2;\\3" _obs_version_canonical ${_obs_version})
+    if(_obs_version MATCHES "^[0-9]+\\.[0-9]+\\.[0-9]+")
+      string(
+        REGEX REPLACE
+        "([0-9]+)\\.([0-9]+)\\.([0-9]+).*"
+        "\\1;\\2;\\3"
+        _obs_version_canonical
+        ${_obs_version}
+      )
+    else()
+      set(_obs_version_canonical ${_obs_default_version})
+      set(_obs_version "${_obs_default_version}")
+    endif()
   endif()
 elseif(DEFINED OBS_VERSION_OVERRIDE)
   if(OBS_VERSION_OVERRIDE MATCHES "([0-9]+)\\.([0-9]+)\\.([0-9]+).*")
